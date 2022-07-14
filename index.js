c1279669044b6b914526881b5f49e88db86d3c82
Andrew Bui <83844505+Andrewbui389@users.noreply.github.com>
2022-07-14 11:04:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const img = document.querySelector('.imageBox');
 let buttonClick = document.querySelector('#newGif')
 buttonClick.addEventListener('click', () =>{
   let searchValue = document.querySelector('#searchResults').value
-  fetch(`https://api.giphy.com/v1/gifs/search?api_key=Hy82xEOEZLNT2Ky2z0S4X4Uy0cNjwMjs&q=${searchValue}&limit=&offset=&rating=r&lang=en`, {mode: 'cors'})
+  fetch(``, {mode: 'cors'})
   .then((response) => {
     return response.json();
   })
